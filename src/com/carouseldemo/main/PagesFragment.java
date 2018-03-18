0c7fd2822365911efebdbb0def6480c5c056b9b1
y2k-shubham <y2k.shubhamgupta@gmail.com>
2018-03-18 19:51:51 +0530

    initial commit

---
+++ src/com/carouseldemo/main/PagesFragment.java
@@ -0,0 +1,22 @@
+package com.carouseldemo.main;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagesFragment extends Fragment {
+
+	   
+    public PagesFragment(){}
+     
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+  
+        View rootView = inflater.inflate(R.layout.pagesfragment, container, false);
+          
+        return rootView;
+    }
+}
---
+++ src/com/carouseldemo/main/Splash.java
@@ -0,0 +1,43 @@
+package com.carouseldemo.main;
+
+//import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+		setContentView(R.layout.activity_splash);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		//getActionBar().hide();
+
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent it = new Intent(
+							"com.carouseldemo.main.navigationdrawer");
+					startActivity(it);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+}
